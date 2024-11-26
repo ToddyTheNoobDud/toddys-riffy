f447e7f5fb125ae64eac84fedcc7838362f901b0
Toddyn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-11-26 15:18:40 -0300

    Faster

--- build/structures/Queue.js
+++ build/structures/Queue.js
@@ -1,33 +1,37 @@
-class Queue extends Array {
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+
     get size() {
-        return this.length;
+        return this.items.length;
     }
 
     get first() {
-        return this.length ? this[0] : null;
+        return this.items.length ? this.items[0] : null;
     }
 
     add(track) {
-        this.push(track);
+        this.items.push(track);
         return this;
     }
 
     remove(index) {
-        if (index >= 0 && index < this.length) {
-            return this.splice(index, 1)[0];
+        if (index >= 0 && index < this.items.length) {
+            return this.items.splice(index, 1)[0];
         } else {
             throw new Error("Index out of range");
         }
     }
 
     clear() {
-        this.length = 0;
+        this.items.length = 0;
     }
 
     shuffle() {
-        for (let i = this.length - 1; i > 0; i--) {
+        for (let i = this.items.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [this[i], this[j]] = [this[j], this[i]];
+            [this.items[i], this.items[j]] = [this.items[j], this.items[i]];
         }
     }
 }
