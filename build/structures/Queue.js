be04a7b64ad7e4ff0b000cc29f8dea09702781b8
Toddyn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-11-26 15:40:10 -0300

    Fix: Track always returning null

--- build/structures/Queue.js
+++ build/structures/Queue.js
@@ -1,37 +1,33 @@
-class Queue {
-    constructor() {
-        this.items = [];
-    }
-
+class Queue extends Array {
     get size() {
-        return this.items.length;
+        return this.length;
     }
 
     get first() {
-        return this.items.length ? this.items[0] : null;
+        return this.length ? this[0] : null;
     }
 
     add(track) {
-        this.items.push(track);
+        this.push(track);
         return this;
     }
 
     remove(index) {
-        if (index >= 0 && index < this.items.length) {
-            return this.items.splice(index, 1)[0];
+        if (index >= 0 && index < this.length) {
+            return this.splice(index, 1)[0];
         } else {
             throw new Error("Index out of range");
         }
     }
 
     clear() {
-        this.items.length = 0;
+        this.length = 0;
     }
 
     shuffle() {
-        for (let i = this.items.length - 1; i > 0; i--) {
+        for (let i = this.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [this.items[i], this.items[j]] = [this.items[j], this.items[i]];
+            [this[i], this[j]] = [this[j], this[i]];
         }
     }
 }
