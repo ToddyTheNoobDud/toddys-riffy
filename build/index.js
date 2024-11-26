0b83a55cd24bb62f6130bfc1330e5dc8d9077d89
Toddyn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-11-26 15:59:10 -0300

    Update index.js

--- build/index.js
+++ build/index.js
@@ -1,11 +1,11 @@
 const { Connection } = require("./structures/Connection");
 const { Filters } = require("./structures/Filters");
 const { Node } = require("./structures/Node");
-const { Riffy } = require("./structures/Riffy");
+const { ToddysRiffy } = require("./structures/Riffy");
 const { Player } = require("./structures/Player");
 const { Plugin } = require("./structures/Plugins");
 const { Queue } = require("./structures/Queue");
 const { Rest } = require("./structures/Rest");
 const { Track } = require("./structures/Track");
 
-module.exports = { Connection, Filters, Node, Riffy, Player, Plugin, Queue, Rest, Track };
+module.exports = { Connection, Filters, Node, ToddysRiffy, Player, Plugin, Queue, Rest, Track };
