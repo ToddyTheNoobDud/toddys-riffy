f24570d178498212ce78d1df3a30ed3b67b6980a
Toddyn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-11-26 15:18:28 -0300

    Memory optimized

--- build/structures/Rest.js
+++ build/structures/Rest.js
@@ -1,13 +1,10 @@
-// destructured, named undiciFetch for Better readability
 const { fetch: undiciFetch, Response } = require("undici");
-const nodeUtil = require("node:util")
+const nodeUtil = require("node:util");
 
 class Rest {
   constructor(riffy, options) {
     this.riffy = riffy;
-    this.url = `http${options.secure ? "s" : ""}://${options.host}:${
-      options.port
-    }`;
+    this.url = `http${options.secure ? "s" : ""}://${options.host}:${options.port}`;
     this.sessionId = options.sessionId;
     this.password = options.password;
     this.version = options.restVersion;
@@ -19,120 +16,80 @@ class Rest {
   }
 
   async makeRequest(method, endpoint, body = null, includeHeaders = false) {
-    const headers = {
+    const headers = new Headers({
       "Content-Type": "application/json",
       Authorization: this.password,
-    };
+    });
 
     const requestOptions = {
       method,
       headers,
       body: body ? JSON.stringify(body) : null,
     };
-    
-    const response = await undiciFetch(this.url + endpoint, requestOptions).catch((e) => {
-      
-      throw new Error(`There was an Error while Making Node Request(likely caused by Network Issue): ${method} ${this.url}${endpoint}`, { cause: e });
-    })
 
-    this.calls++;
+    let response;
+    try {
+      response = await undiciFetch(`${this.url}${endpoint}`, requestOptions);
+      this.calls++;
+    } catch (e) {
+      throw new Error(`Error making Node Request (likely caused by Network Issue): ${method} ${this.url}${endpoint}`, { cause: e });
+    }
 
-    // Parses The Request
     const data = await this.parseResponse(response);
-
-    // Emit apiResponse event with important data and Response
     this.riffy.emit("apiResponse", endpoint, response);
+    this.logRequest(method, endpoint, body, response, data);
 
+    return includeHeaders ? { data, headers: response.headers } : data;
+  }
+
+  logRequest(method, endpoint, body, response, data) {
     this.riffy.emit(
       "debug",
-      `[Rest] ${requestOptions.method} ${
-        endpoint.startsWith("/") ? endpoint : `/${endpoint}`
-      } ${body ? `body: ${JSON.stringify(body)}` : ""} -> \n Status Code: ${
-        response.status
-      }(${response.statusText}) \n Response(body): ${JSON.stringify(await data)} \n Headers: ${
-        nodeUtil.inspect(response.headers)
-      }`
+      `[Rest] ${method} ${endpoint.startsWith("/") ? endpoint : `/${endpoint}`} ${body ? `body: ${JSON.stringify(body)}` : ""} -> \n Status Code: ${response.status} (${response.statusText}) \n Response(body): ${JSON.stringify(data)} \n Headers: ${nodeUtil.inspect(response.headers)}`
     );
-
-    return includeHeaders === true ? {
-      data,
-      headers: response.headers,
-    } : data;
   }
 
   async getPlayers() {
-    return this.makeRequest(
-      "GET",
-      `/${this.version}/sessions/${this.sessionId}/players`
-    );
+    return this.makeRequest("GET", `/${this.version}/sessions/${this.sessionId}/players`);
   }
 
   async updatePlayer(options) {
-    // destructure data as requestBody for ease of use.
     let { data: requestBody } = options;
 
     if (
-      (typeof requestBody.track !== "undefined" &&
-        requestBody.track.encoded &&
-        requestBody.track.identifier) ||
+      (requestBody.track && requestBody.track.encoded && requestBody.track.identifier) ||
       (requestBody.encodedTrack && requestBody.identifier)
-    )
-      throw new Error(
-        `${
-          typeof requestBody.track !== "undefined"
-            ? `encoded And identifier`
-            : `encodedTrack And identifier`
-        } are mutually exclusive (Can't be provided together) in Update Player Endpoint`
-      );
+    ) {
+      throw new Error(`encoded And identifier are mutually exclusive in Update Player Endpoint`);
+    }
 
     if (this.version === "v3" && options.data?.track) {
       const { track, ...otherRequestData } = requestBody;
-
       requestBody = { ...otherRequestData };
-
       Object.assign(
         options.data,
-        typeof options.data.track.encoded !== "undefined"
-          ? { encodedTrack: track.encoded }
-          : { identifier: track.identifier }
+        track.encoded !== undefined ? { encodedTrack: track.encoded } : { identifier: track.identifier }
       );
     }
 
-    return this.makeRequest(
-      "PATCH",
-      `/${this.version}/sessions/${this.sessionId}/players/${options.guildId}?noReplace=false`,
-      options.data
-    );
+    return this.makeRequest("PATCH", `/${this.version}/sessions/${this.sessionId}/players/${options.guildId}?noReplace=false`, options.data);
   }
 
   async destroyPlayer(guildId) {
-    return this.makeRequest(
-      "DELETE",
-      `/${this.version}/sessions/${this.sessionId}/players/${guildId}`
-    );
+    return this.makeRequest("DELETE", `/${this.version}/sessions/${this.sessionId}/players/${guildId}`);
   }
 
   async getTracks(identifier) {
-    return this.makeRequest(
-      "GET",
-      `/${this.version}/loadtracks?identifier=${encodeURIComponent(identifier)}`
-    );
+    return this.makeRequest("GET", `/${this.version}/loadtracks?identifier=${encodeURIComponent(identifier)}`);
   }
 
   async decodeTrack(track, node) {
     if (!node) node = this.leastUsedNodes[0];
-    return this.makeRequest(
-      `GET`,
-      `/${this.version}/decodetrack?encodedTrack=${encodeURIComponent(track)}`
-    );
+    return this.makeRequest("GET", `/${this.version}/decodetrack?encodedTrack=${encodeURIComponent(track)}`);
   }
 
   async decodeTracks(tracks) {
-    return this.makeRequest(
-      `POST`,
-      `/${this.version}/decodetracks`,
-      tracks
-    );
+    return this.makeRequest("POST", `/${this.version}/decodetracks`, tracks);
   }
 
   async getStats() {
@@ -144,21 +101,15 @@ class Rest {
   }
 
   async getRoutePlannerStatus() {
-    return this.makeRequest(
-      `GET`,
-      `/${this.version}/routeplanner/status`
-    );
+    return this.makeRequest("GET", `/${this.version}/routeplanner/status`);
   }
+
   async getRoutePlannerAddress(address) {
-    return this.makeRequest(
-      `POST`,
-      `/${this.version}/routeplanner/free/address`,
-      { address }
-    );
+    return this.makeRequest("POST", `/${this.version}/routeplanner/free/address`, { address });
   }
 
   /**
-   * @description Parses The Process Request and Performs necessary Checks(if statements)
+   * @description Parses The Process Request and Performs necessary Checks
    * @param {Response} req
    * @returns {object | null}
    */
@@ -166,16 +117,11 @@ class Rest {
     if (req.status === 204) {
       return null;
     }
-
     try {
-      return await req[req.headers.get("Content-Type").includes("text/plain") ? "text" : "json"]();
+      const contentType = req.headers.get("Content-Type");
+      return await req[contentType.includes("text/plain") ? "text" : "json"]();
     } catch (e) {
-      this.riffy.emit(
-        "debug",
-        `[Rest - Error] There was an Error for ${
-          new URL(req.url).pathname
-        } ${e}`
-      );
+      this.riffy.emit("debug", `[Rest - Error] There was an Error for ${new URL(req.url).pathname} ${e}`);
       return null;
     }
   }
